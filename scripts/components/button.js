51c0a8423c1998abd309a1c1e4485f1e06e369c6
Alisher <alisher.baudinovv@gmail.com>
2024-03-13 23:31:38 +0500

    MAC-06 feat: implement few btns components

--- scripts/components/button.js
+++ scripts/components/button.js
@@ -1,15 +1,13 @@
-const classTypes = {
-	default: 'btn-default',
-	outline: 'btn-outline',
-}
-
+const classTypes = ['btn-default', 'btn-outline', 'btn-icon', 'btn-border'];
 customElements.define('mac-button', class MacButton extends HTMLElement { // component name
 	constructor() {
 		super();
 
 		const text = this.getAttribute('data-content'); // get the data-content attribute
-		const type = classTypes[this.getAttribute('data-view') || 'default']; // get the data-view attribute and math to object classTypes
+		const type = (classTypes.includes(this.getAttribute('data-view'))) ? this.getAttribute('data-view') : console.error(` There is no custom class like "${this.tagName}".\n Only "${classTypes}" `); // get the data-view attribute and math to object classTypes
+		const icon = ``
+		const customClass = '';
 		this.innerHTML = 
-		`<button class='btn ${type}'>${text}</button>` // render html tags
+		`<button class='btn ${type} ${customClass}'>${icon}<span>${text}</span></button>` // render html tags
 	}
 });
