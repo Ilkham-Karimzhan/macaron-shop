8ddb61a02d79c3f556c05488c0cbcba5a60e2042
Ilkham-Karimzhan <ilhamkar00@gmail.com>
2024-03-14 23:19:58 +0600

    MAC-07 fix(checkbox): disabled & id attrubites added

--- scripts/components/checkbox.js
+++ scripts/components/checkbox.js
@@ -1,16 +1,35 @@
-const status = {
-   default: "btn-default",
- };
- customElements.define(
-   "mac-checkbox",
-   class MacCheckbox extends HTMLElement {
-     constructor() {
-       super();
- 
-       this.innerHTML = `<label class="con">
-       <input type="checkbox"  />
-       <span class="checkmark"></span>
-       </label>`;
-     }
-   }
- );
+function check(state) {
+  if (state == 'true') {
+    return 'disabled';
+  } else {
+    return '';
+  }
+}
+
+customElements.define(
+  'mac-checkbox',
+  class MacCheckbox extends HTMLElement {
+    constructor() {
+      super();
+
+      let isDisabled = this.getAttribute('disabled');
+      let id = this.getAttribute('id');
+
+      const label = document.createElement('label');
+      label.classList.add('con');
+
+      const input = document.createElement('input');
+      input.setAttribute('type', 'checkbox');
+      input.setAttribute('id', id);
+      input.disabled = isDisabled === 'true';
+
+      const span = document.createElement('span');
+      span.classList.add('checkmark');
+
+      label.appendChild(input);
+      label.appendChild(span);
+
+      this.appendChild(label);
+    }
+  }
+);
