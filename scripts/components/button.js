e1e045003e9ca46bbfad00d1b8bf910895491325
Alisher <alisher.baudinovv@gmail.com>
2024-03-14 21:49:03 +0500

    MAC-06 fix: btn complements

--- scripts/components/button.js
+++ scripts/components/button.js
@@ -1,15 +1,14 @@
-const classTypes = {
-	default: 'btn-default',
-	outline: 'btn-outline',
-}
+const classTypes = ['btn-default', 'btn-outline'];
 
 customElements.define('mac-button', class MacButton extends HTMLElement { // component name
 	constructor() {
 		super();
 
 		const text = this.getAttribute('data-content'); // get the data-content attribute
-		const type = classTypes[this.getAttribute('data-view') || 'default']; // get the data-view attribute and math to object classTypes
+		const type = (classTypes.includes(this.getAttribute('data-view'))) ? this.getAttribute('data-view') : console.error(`There is no custom class like ${this.className} in "${this.tagName}".\nOnly "${classTypes}" `); // get the data-view attribute and math to object classTypes
+		const icon = ``;
+		const customClass = this.getAttribute('data-custom-class');
 		this.innerHTML = 
-		`<button class='btn ${type}'>${text}</button>` // render html tags
+		`<button class='btn ${type} ${customClass}'>${icon}<span>${text}</span></button>` // render html tags
 	}
 });
