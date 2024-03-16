5c5f13fc6999c97b3871e02a78c9b509415fc52d
A4U4UK <beket9583@gmail.com>
2024-03-16 18:04:52 +0500

    MAC-09 feat: add custom radio button

---
+++ scripts/components/radiobutton.js
@@ -0,0 +1,37 @@
+function check(state) {
+  if (state == 'true') {
+    return 'disabled';
+  } else {
+    return '';
+  }
+}
+
+customElements.define(
+  'mac-radio-button',
+  class MacRadioButton extends HTMLElement {
+    constructor() {
+      super();
+
+      let isDisabled = this.getAttribute('disabled');
+      let id = this.getAttribute('id');
+      let name = this.getAttribute('name');
+
+      const label = document.createElement('label');
+      label.classList.add('custom-radio');
+
+      const input = document.createElement('input');
+      input.setAttribute('type', 'radio');
+      input.setAttribute('id', id);
+      input.setAttribute('name', name);
+      input.disabled = isDisabled === 'true';
+
+      const span = document.createElement('span');
+      span.classList.add('radio-button');
+
+      label.appendChild(input);
+      label.appendChild(span);
+
+      this.appendChild(label);
+    }
+  }
+);
