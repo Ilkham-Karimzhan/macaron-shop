232fd44e9128709712cbcb0c93f6389205363b74
Alisher <alisher.baudinovv@gmail.com>
2024-03-21 23:11:06 +0500

    MAC-10 feat: implement header

---
+++ scripts/components/header.js
@@ -0,0 +1,9 @@
+customElements.define('mac-header', class MacButton extends HTMLElement { // component name
+	constructor() {
+		super();
+
+        const customClass = this.getAttribute('data-custom-class');
+		this.innerHTML = 
+        ''
+	}
+});
